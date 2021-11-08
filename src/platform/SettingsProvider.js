5cda1f9aa8095fb45a8896c8b04a6d58ad405832
Dory Azar <dory.azar@gmail.com>
2021-11-08 10:57:19 -0500

    Fixing time order

--- src/platform/SettingsProvider.js
+++ src/platform/SettingsProvider.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { TIMER_SETTINGS, T_STOPWATCH, saveToStorage } from '../utils/helpers';
+import { TIMER_SETTINGS, T_STOPWATCH, T_COUNTDOWN, saveToStorage } from '../utils/helpers';
 
 export const SettingsContext = React.createContext({});
 
@@ -23,15 +23,30 @@ export const SettingsProvider = ({ children }) => {
 
     // Setter that sets all the settings in one call
     const setSettings = useCallback((settings) =>  {
+
+        // Switch start and end if order not respected
+        let newStartTime = settings.startTime;
+        let newStopTime = settings.stopTime;
+        if ((selectedTimer === T_STOPWATCH && settings.startTime > settings.stopTime)
+            || (selectedTimer === T_COUNTDOWN && settings.startTime < settings.stopTime)) {
+            newStartTime = settings.stopTime;
+            newStopTime =  settings.startTime;
+        } 
+
+        // save if persistent is turned on
         if (isPersistent()) saveToStorage(selectedTimer, settings);
-        setStartTime(settings.startTime && isValid(settings.startTime)? parseInt(settings.startTime): 0);
-        setStopTime(settings.stopTime && isValid(settings.stopTime)? parseInt(settings.stopTime): 0);
+
+        // Set the settings
+        setStartTime(settings.startTime && isValid(settings.startTime)? parseInt(newStartTime): 0);
+        setStopTime(settings.stopTime && isValid(settings.stopTime)? parseInt(newStopTime): 0);
         setTotalRounds(settings.totalRounds && isValid(settings.totalRounds, "rounds")? parseInt(settings.totalRounds):  1);
         setRestStartTime(settings.restStartTime && isValid(settings.restStartTime) ? parseInt(settings.restStartTime): 0);
     }, [selectedTimer])
 
+
     // Checks if input is valid based on expected configurations in timer.json
     const isValid =  (value, mode = "time") =>  value && !isNaN(value) && value <= TIMER_SETTINGS.configurations[mode].max && value >= TIMER_SETTINGS.configurations[mode].min;
+
     
     // Checks if persistence is turned on in the configurations
     const isPersistent = () => TIMER_SETTINGS.configurations.persistence;
